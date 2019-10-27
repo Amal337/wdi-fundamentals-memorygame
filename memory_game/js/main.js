91b08fc5588b73b0b00a78fd113920eb493f9814
Amal337 <39064067+Amal337@users.noreply.github.com>
2019-10-27 08:12:53 +0300

    created logic game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,27 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png",
+},
+{
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png",
+
+},
+{
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png",
+},
+{
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png",
+}
+];
+
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -11,12 +34,14 @@ function checkForMatch() {
 
 function flipCard(cardId) {
 
-    cardsInPlay.push(cards[cardId]);
+    cardsInPlay.push(cards[cardId.rank]);
 
   if(cardsInPlay.length === 2) {
      checkForMatch();
 }
-     console.log("User flipped " + cards[cardId]);
+     console.log("User flipped " + cards[cardId].rank);
+     console.log(cards[cardId].cardImage);
+     console.log(cards[cardId].suit);
 }
 
 
